6058c76ca7dc66d9088c3bb374bddf90b00ec815
EvgenyFirst <>
2021-03-21 16:38:32 +0600

    редактирование секции Отзывы о «Шведской       горечи»

---
+++ js/reviews-slider.js
@@ -0,0 +1,56 @@
+// Слайдер в секции "Сервисы" start
+
+const changeSection9rewievsHeaderRus = document.querySelector(".section-9__rewievs-header-rus");
+const changeSection9rewievsHeaderEng = document.querySelector(".section-9__rewievs-header-eng");
+
+const changeSection9rewievsHeaderTxtRus = document.querySelector(".section-9__rewievs-header-txt-rus");
+const changeSection9rewievsHeaderTxtEng = document.querySelector(".section-9__rewievs-header-txt-eng");
+
+const changesection9rewievsArticleRus = document.querySelector(".section-9__rewievs-article-rus");
+const changesection9rewievsArticleEng = document.querySelector(".section-9__rewievs-article-eng");
+
+const changeSection9rewievsHidden = document.querySelector(".section-9__rewievs-hidden");
+
+/* Открывает и закрывает слайды в разделе отзывов,
+а также меняет цвета текста и фонов на переключателях слайдов start */
+changeSection9rewievsHeaderEng.addEventListener("click", function () {
+  changeSection9rewievsHeaderEng.classList.remove("section-9__rewievs-bg-header-active");
+  changeSection9rewievsHeaderEng.classList.add("section-9__rewievs-bg-header-not-active");
+
+  changeSection9rewievsHeaderTxtRus.classList.remove("section-9__rewievs-header-txt-active");
+  changeSection9rewievsHeaderTxtRus.classList.add("section-9__rewievs-header-color-not-active");
+
+  changeSection9rewievsHeaderTxtEng.classList.remove("section-9__rewievs-header-color-not-active");
+  changeSection9rewievsHeaderTxtEng.classList.add("section-9__rewievs-header-txt-active");
+
+  changeSection9rewievsHeaderRus.classList.remove("section-9__rewievs-bg-header-active");
+  changeSection9rewievsHeaderRus.classList.add("section-9__rewievs-bg-header-not-active");
+
+  changeSection9rewievsHeaderTxtEng.classList.remove("section-9__rewievs-header-color-not-active");
+  changeSection9rewievsHeaderTxtEng.classList.add("section-9__rewievs-header-txt-active");
+
+  changeSection9rewievsHeaderTxtEng.classList.add("section-9__rewievs-bg-header-active");
+
+  changesection9rewievsArticleRus.classList.add("section-9__rewievs-hidden");
+  changesection9rewievsArticleEng.classList.remove("section-9__rewievs-hidden");
+});
+
+changeSection9rewievsHeaderRus.addEventListener("click", function () {
+  changeSection9rewievsHeaderRus.classList.add("section-9__rewievs-bg-header-active");
+  changeSection9rewievsHeaderRus.classList.remove("section-9__rewievs-bg-header-not-active");
+
+  changeSection9rewievsHeaderTxtRus.classList.add("section-9__rewievs-header-txt-active");
+  changeSection9rewievsHeaderTxtRus.classList.remove("section-9__rewievs-header-color-not-active");
+
+  changeSection9rewievsHeaderTxtEng.classList.add("section-9__rewievs-header-color-not-active");
+  changeSection9rewievsHeaderTxtEng.classList.remove("section-9__rewievs-header-txt-active");
+
+  changeSection9rewievsHeaderTxtEng.classList.remove("section-9__rewievs-bg-header-active");
+
+  changesection9rewievsArticleRus.classList.remove("section-9__rewievs-hidden");
+  changesection9rewievsArticleEng.classList.add("section-9__rewievs-hidden");
+
+  });
+/* Открывает и закрывает слайды в разделе отзывов,
+а также меняет цвета текста и фонов на переключателях слайдов end */
+
